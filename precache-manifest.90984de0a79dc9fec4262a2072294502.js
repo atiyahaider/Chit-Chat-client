4e8261e39efef268beff5c607c830a53cc671a2e
Atiya Haider <atiya_haider@hotmail.com>
2020-08-31 12:24:58 -0400

    Updates

---
+++ precache-manifest.90984de0a79dc9fec4262a2072294502.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9babef56324a32fe92ef1abcf69dfcb",
+    "url": "/Chit-Chat-client/index.html"
+  },
+  {
+    "revision": "9de5bca0b62dda9e7993",
+    "url": "/Chit-Chat-client/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "3335e468cfd6be78eb92",
+    "url": "/Chit-Chat-client/static/css/main.5a9426a2.chunk.css"
+  },
+  {
+    "revision": "9de5bca0b62dda9e7993",
+    "url": "/Chit-Chat-client/static/js/2.f32d2a7d.chunk.js"
+  },
+  {
+    "revision": "7f94472d52bc5ff481f1aa081bb37a90",
+    "url": "/Chit-Chat-client/static/js/2.f32d2a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3335e468cfd6be78eb92",
+    "url": "/Chit-Chat-client/static/js/main.ad8725fd.chunk.js"
+  },
+  {
+    "revision": "0ea4289909a2c91ea97b",
+    "url": "/Chit-Chat-client/static/js/runtime-main.7b1200ad.js"
+  }
+]);
